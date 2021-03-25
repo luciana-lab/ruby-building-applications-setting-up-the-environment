9bf533f2e2e42b1d4801c34e50d36c98687e7859
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-25 17:33:36 -0400

    commit tree 5db181d042668bb1c1accc805c76c293eac016c8

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,9 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+
+#Environment.rb typically loaded when an application is started or tests are run, 
+#this file loads any required application files and handles any configuration that has to happen every time the application starts.
+
+require_relative '../lib/garden.rb'
+require_relative '../lib/plant.rb'
